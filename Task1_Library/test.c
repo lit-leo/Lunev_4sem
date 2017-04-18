1566846f1eaf76f5f2b86ef36c6855134b327605
litleo <litleo@mail.ru>
2017-04-18 16:22:22 +0300

    Uniqe entries picking of hashFunc test

--- Task1_Library/test.c
+++ Task1_Library/test.c
@@ -3,12 +3,23 @@
 
 int main(int argc, char** argv)
 {
-    hashTable_t test;
-    hashTableCtor(&test, 16);
-    char ex[15] = "JA DISHU POD X";
+    /*hashTable_t test;
+    hashTableCtor(&test, 19000);
     unsigned int i = hashTableInsert(&test, (char*)(&ex));
     
-    printf("%s\n", (test.table)[i]);
+    printf("%s\n", (test.table)[i]);*/
+    char ex[15] = "JA DISHU POD X";
 
+    int top = 2097152;
+    bitArray_t indicator;
+    bitArrayCtor(&indicator, top);
+    for(int i = 0; i < top; i++)
+        bitArraySet(&indicator, hashFunc((char*)(&ex), i, top));
+
+    for (int i = 0; i < indicator.size; ++i)
+    {
+        if(bitArrayTest(&indicator, i) == 0)
+            printf("CHEEKE BREEKE I V DAMKE\n");
+    }
     return 0;
 }
